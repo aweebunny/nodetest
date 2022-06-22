17f646e4194032657681ace4411299e60cb92239
aweebunny <39164164+aweebunny@users.noreply.github.com>
2022-06-21 19:23:22 -0500

    Add intial files

---
+++ public/test.js
@@ -0,0 +1,12 @@
+//fetch works, see console.log
+// fetch('https://pokeapi.co:443/api/v2/pokemon/44/').then(function (response) {
+//     // The API call was successful!
+//     console.log('success!', response);
+// }).catch(function (err) {
+//     // There was an error
+//     console.warn('Something went wrong.', err);
+// });
+
+import { Pokedex } from "../pokedex-promise-v2.js";
+
+
